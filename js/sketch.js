ff254bd1b1f1eee85664c039217279e58e58cf7b
LieveOtten <71327022+LieveOtten@users.noreply.github.com>
2020-11-12 09:43:57 +0100

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,231 @@
+function setup() {
+  createCanvas(500,500);
+  background(252,227,3);
+
+  //TWEEDE GEBOUW
+  noStroke();
+  fill(219, 203, 50);
+  rect(250,25,200,475);
+  
+  //TWEEDE GEBOUW KLEUR
+  noStroke();
+  fill(74, 72, 50);
+  rect(350,25,100,475);
+
+ //RAAM EEN GEBOUW TWEE
+ noStroke();
+ fill(18,17,9);
+ rect(270,50,160,25);
+
+ //RAAM TWEE GEBOUW TWEE
+ noStroke();
+ fill(41,39,28);
+ rect(270,95,160,25);
+
+ //RAAM DRIE GEBOUW TWEE 
+ noStroke();
+ fill(18,17,9);
+ rect(270,140,160,25);
+
+ //RAAM VIER GEBOUW TWEE
+ noStroke();
+ fill(41,39,28);
+ rect(270,185,160,25);
+
+ //RAAM VIJF GEBOUW TWEE
+ noStroke();
+ fill(18,17,9);
+ rect(270,230,160,25);
+
+ //RAAM ZES GEBOUW TWEE
+ noStroke();
+ fill(41,39,28);
+ rect(270,275,160,25);
+
+ //RAAM ZEVEN GEBOUW TWEE
+ noStroke();
+ fill(18,17,9);
+ rect(270,320,160,25);
+
+ //RAAM ACHT GEBOUW TWEE
+ noStroke();
+ fill(41,39,28);
+ rect(270,365,160,25);
+
+ //RAAM NEGEN GEBOUW TWEE
+ noStroke();
+ fill(18,17,9);
+ rect(270,410,160,25);
+
+ //BALK TWEE GEBOUW EEN 
+noStroke();
+fill(20,50);
+rect(315,380,10,150);
+
+
+//DERDE GEBOUW
+noStroke();
+fill(2242, 226, 78);
+rect(220,380,100,350);
+
+//DERDE GEBOUW BALLK
+noStroke();
+fill(18,17,9);
+rect(220,380,100,12.5);
+
+//DERDE GEBOUW RAAM
+noStroke();
+fill(18,17,9);
+rect(220,405,90,15);
+
+//RAAM
+noStroke();
+fill(18,17,9);
+rect(220,435,90,15);
+
+//RAAM 
+noStroke();
+fill(18,17,9);
+rect(220,465,90,15);
+
+//SCHOORSTEEN
+noStroke();
+fill(18,17,9);
+rect(130,110,85,100);
+
+
+//EERSTE GEBOUW
+noStroke();
+fill(184, 171, 55);
+rect(25,150,200,350);
+
+//RAAM GEBOUW EEN
+noStroke();
+fill(41,39,28);
+rect(50,175,150,25);
+
+//RAAM TWEE GEBOUW EEN 
+noStroke();
+fill(41,39,28);
+rect(50,475,150,25);
+
+//KLEIN RAAM GEBOUW EEN 
+noStroke();
+fill(41,39,28);
+rect(70,350,25,25);
+
+//KLEIN RAAM TWEE GEBOUW EEN 
+noStroke();
+fill(41,39,28);
+rect(100,350,25,25);
+
+//KLEIN RAAM DRIE GEBOUW EEM
+noStroke();
+fill(41,39,28);
+rect(130,350,25,25);
+
+//KLEIN RAAM VIER GEBOUW EEN 
+noStroke();
+fill(41,39,28);
+rect(160,350,25,25);
+
+ //KLEIN RAAM VIJF GEBOUW EEN
+ noStroke();
+ fill(41,39,28);
+ rect(70,380,25,25);
+
+ //KLEIN RAAM ZES GEBOUW EEN 
+ noStroke();
+ fill(41,39,28);
+ rect(100,380,25,25);
+
+ //KLEIN RAAM ZEVEN GEBOUW EEN
+ noStroke();
+ fill(41,39,28);
+ rect(130,380,25,25);
+
+ //KLEIN RAAM ACHT GEBOUW EEN
+ noStroke();
+ fill(41,39,28);
+ rect(160,380,25,25);
+
+ //KLEIN RAAM NEGEN GEBOUW EEN 
+ noStroke();
+ fill(41,39,28);
+ rect(70,410,25,25);
+
+ //KLEIN RAAM TIEN GEBOUW EEN 
+ noStroke();
+ fill(41,39,28);
+ rect(100,410,25,25);
+
+ //KLEIN RAAM ELF GEBOUW EEN
+ noStroke();
+ fill(41,39,28);
+ rect(130,410,25,25);
+
+ //KLEIN RAAM TWAALF GEBOUW EEN 
+ noStroke();
+ fill(41,39,28);
+ rect(160,410,25,25);
+
+//BALK GEBOUW EEN 
+noStroke();
+fill(18,17,9);
+rect(12.5,215,225,50);
+
+//BALK TWEE GEBOUW EEN
+noStroke();
+fill(41,39,28);
+rect(12.5,280,225,50);
+
+
+//DERDE GEBOUW 
+stroke(18,17,9);
+strokeWeight(3);
+fill(18,17,9);
+ellipse(420,305,100,80);
+
+//VIERDE GEBOUW
+noStroke();
+fill(224, 208, 52);
+rect(350,300,140,350);
+
+//BALK TWEE GEBOUW VIER
+noStroke();
+fill(115, 112, 81);
+rect(350,300,140,8);
+
+//BALK TWEE GEBOUW VIER
+noStroke();
+fill(41,39,28);
+rect(375,330,20,150);
+
+//BALK TWEE GEBOUW VIER
+noStroke();
+fill(41,39,28);
+rect(410,330,20,150);
+
+//BALK TWEE GEBOUW VIER
+noStroke();
+fill(41,39,28);
+rect(445,330,20,150);
+
+//BALK TWEE GEBOUW EEN 
+noStroke();
+fill(20,50);
+rect(225,380,10,150);
+
+//BALK TWEE GEBOUW EEN
+noStroke();
+fill(20,100);
+rect(337,300,13,200);
+
+fill(115, 112, 81);
+text("LIEVE OTTEN",100,60);
+
+
+}
+
+function draw() {
+}
